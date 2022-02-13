875511c682d08b40f35c910fe2c9c905c6ef414f
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-13 18:16:26 +0100

    Minor addons to the Film table in the DB

--- invoiceweb/models.py
+++ invoiceweb/models.py
@@ -4,6 +4,13 @@ class Film(models.Model):
     # documentation: https://docs.djangoproject.com/en/4.0/ref/models/fields/
     tytul = models.CharField(max_length=64, blank=False, unique=True)
     rok = models.PositiveSmallIntegerField(default=2000)
+    opis = models.TextField(default="")
+    premiera = models.DateField(null=True, blank=True)
+    imdb_rating = models.DecimalField(max_digits=4, decimal_places=2)
+    plakat = models.ImageField(upload_to="plakaty", null=True, blank=True)
 
     def __str__(self):
-        return self.tytul + " (" + str(self.rok) + ")"
+        return self.tytul_z_rokiem()
+
+    def tytul_z_rokiem(self):
+        return "{} ({})".format(self.tytul, self.rok)
