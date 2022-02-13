1a35ad342eca6247eab6e81cd051bf4cd53c4c5b
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-13 10:20:21 +0100

    Adding data base and first record

--- invoiceweb/models.py
+++ invoiceweb/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Film(models.Model):
+    # documentation: https://docs.djangoproject.com/en/4.0/ref/models/fields/
+    tytul = models.CharField(max_length=64, blank=False, unique=True)
+    rok = models.PositiveSmallIntegerField(default=2000)
+
+    def __str__(self):
+        return self.tytul + " (" + str(self.rok) + ")"
