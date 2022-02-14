527dce76b928f502970f5ab092a4e8fb4f729a60
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-14 21:49:59 +0100

    CRUD 2

--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from filmyweb.views import wszystkie_filmy, nowy_film, edytuj_film
+from filmyweb.views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 
 
 urlpatterns = [
     path('wszystkie/', wszystkie_filmy),
     path('nowy/', nowy_film),
     path('edytuj/<int:id>/', edytuj_film),
+    path('usun/<int:id>/', usun_film),
 ]
