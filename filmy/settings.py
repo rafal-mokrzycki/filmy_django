22cb66b46a34a93518a419af70de2869cd8d9b67
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-15 19:06:53 +0100

    Adding login-logout upgrades

--- filmy/settings.py
+++ filmy/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = ['moje_static']
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/moje_media/'
 
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'wszystkie_filmy'
+LOGOUT_REDIRECT_URL = 'login'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- filmyweb/views.py
+++ filmyweb/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
 from .models import Film
 from .forms import FilmForm
+from django.contrib.auth.decorators import login_required
 
 
 def wszystkie_filmy(request):
     wszystkie = Film.objects.all()
     return render(request, 'filmy.html', {'filmy': wszystkie})
 
-
+@login_required
 def nowy_film(request):
     form = FilmForm(request.POST or None, request.FILES or None)
 
@@ -18,7 +18,7 @@ def nowy_film(request):
 
     return render(request, 'film_form.html', {'form': form})
 
-
+@login_required
 def edytuj_film(request, id):
     film = get_object_or_404(Film, pk=id)
     form = FilmForm(request.POST or None, request.FILES or None, instance=film)
