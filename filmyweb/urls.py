b0c5b0ecbfda1aeb4d70d934943b190a57b29e8f
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-15 21:42:27 +0100

    Further stylization with CSS (bootstrap and fontawesome) 3

--- filmyweb/urls.py
+++ filmyweb/urls.py
@@ -5,6 +5,6 @@ from filmyweb.views import wszystkie_filmy, nowy_film, edytuj_film, usun_film
 urlpatterns = [
     path('wszystkie/', wszystkie_filmy, name="wszystkie_filmy"),
     path('nowy/', nowy_film, name="nowy_film"),
-    path('edytuj/<int:invoice_number>/', edytuj_film, name="edytuj_film"),
-    path('usun/<int:invoice_number>/', usun_film, name="usun_film"),
+    path('edytuj/<int:id>/', edytuj_film, name="edytuj_film"),
+    path('usun/<int:id>/', usun_film, name="usun_film"),
 ]
