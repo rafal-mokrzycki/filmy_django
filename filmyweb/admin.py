fb8491bace9daeea5bd65ce00e9c9958ec6623fb
Rafał Mokrzycki <55636265+rafal-mokrzycki@users.noreply.github.com>
2022-02-16 20:53:45 +0100

    Delete filmyweb directory

--- filmyweb/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Film
-
-# admin.site.register(Film)
-
-@admin.register(Film)
-class FilmAdmin(admin.ModelAdmin):
-    pass
--- filmyweb/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Film
-
-class FilmForm(ModelForm):
-    class Meta:
-        model = Film
-        fields = ['tytul', 'opis', 'premiera', 'rok', 'imdb_rating', 'plakat']
